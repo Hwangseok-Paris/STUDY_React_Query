506e169ddc70b362f2676c8ff901b83485388d18
paris <hwangseok.paris@gmail.com>
2024-04-02 09:52:56 +0900

    스터디 2회차 - refetch

--- react-query-test/src/ReactQuery.jsx
+++ react-query-test/src/ReactQuery.jsx
@@ -6,13 +6,19 @@ const fetchProducts = () => {
 };
 
 export const ReactQuery = () => {
-  const { isLoading, data, isError, error } = useQuery("get-product", fetchProducts);
+  const { isLoading, isFetching, data, isError, error } = useQuery("get-product", fetchProducts, {
+    // staleTime: 3000,
+    // refetchOnMount: true, // mount시에 fetch 작동
+    // refetchOnWindowFocus: true, // 유저가 해당 컴포넌트에 포커스 했을 경우 refetch 실행
+    // refetchInterval: 2000,  // 사용자의 동작에 관계 없이 지정된 시간마다 refetch(default - false)
+    // refetchIntervalInBackground: true,  //  ture로 지정했을 경우에는 백그라운드에 있을 때에도 refetch(default - false)
+  });
 
+  console.log({ isLoading, isFetching });
   if (isLoading) return <>Loading...</>;
   if (isError) return <>{error.message}</>;
 
   if (data.data.error == 500) {
-    console.log(isError);
     return <div className="text-[red]">{data.data?.message}</div>;
   }
 
