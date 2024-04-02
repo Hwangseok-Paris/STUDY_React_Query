cf0548f9ad6f1f5cd19a8a0ea8ba140f6ba488c0
paris <hwangseok.paris@gmail.com>
2024-04-02 11:15:16 +0900

    스터디 3회차 - 클릭시 fetch, 커스텀 콜백, 데이터 변환, 커스텀 훅

--- react-query-test/src/ReactQuery.jsx
+++ react-query-test/src/ReactQuery.jsx
@@ -1,24 +1,43 @@
-import { useQuery } from "react-query";
-import axios from "axios";
+// import { useQuery } from "react-query";
+// import axios from "axios";
+import { useProductName } from "./hooks/useProductName";
 
-const fetchProducts = () => {
-  return axios.get("https://api.sampleapis.com/wines/reds");
-};
+// const fetchProducts = () => {
+//   return axios.get("https://api.sampleapis.com/wines/reds");
+// };
 
 export const ReactQuery = () => {
-  const { isLoading, isFetching, data, isError, error } = useQuery("get-product", fetchProducts, {
-    // staleTime: 3000,
-    // refetchOnMount: true, // mount시에 fetch 작동
-    // refetchOnWindowFocus: true, // 유저가 해당 컴포넌트에 포커스 했을 경우 refetch 실행
-    // refetchInterval: 2000,  // 사용자의 동작에 관계 없이 지정된 시간마다 refetch(default - false)
-    // refetchIntervalInBackground: true,  //  ture로 지정했을 경우에는 백그라운드에 있을 때에도 refetch(default - false)
-  });
+  const onSuccess = (data) => {
+    console.log("데이터 가져오기 후 사이드 이펙트 수행", data);
+  };
+
+  const onError = (data) => {
+    console.log("오류 발생 후 오류 이펙트 수행", data);
+  };
+
+  const { isLoading, isFetching, data, isError, error } = useProductName(onSuccess, onError);
+  // const { isLoading, isFetching, data, isError, error } = useQuery("get-product", fetchProducts, {
+  //   // staleTime: 3000,
+  //   // refetchOnMount: true, // mount시에 fetch 작동
+  //   // refetchOnWindowFocus: true, // 유저가 해당 컴포넌트에 포커스 했을 경우 refetch 실행
+  //   // refetchInterval: 2000,  // 사용자의 동작에 관계 없이 지정된 시간마다 refetch(default - false)
+  //   // refetchIntervalInBackground: true,  //  ture로 지정했을 경우에는 백그라운드에 있을 때에도 refetch(default - false)
+  //   // enabled: false,
+  //   onSuccess, // 성공 콜백
+  //   onError, // 실패 콜백
+  //   select: (data) => {
+  //     const productName = data.data?.map((p) => p.wine).slice(0, 9);
+  //     return productName;
+  //   },
+  //   // onSuccess: onSuccess,
+  //   // onError: onError,
+  // });
 
   console.log({ isLoading, isFetching });
   if (isLoading) return <>Loading...</>;
   if (isError) return <>{error.message}</>;
 
-  if (data.data.error == 500) {
+  if (data && data.data?.error == 500) {
     return <div className="text-[red]">{data.data?.message}</div>;
   }
 
@@ -26,13 +45,18 @@ export const ReactQuery = () => {
     <>
       <div className="text-4xl">React Query</div>
 
+      {/* <button onClick={refetch} className="py-2 px-4 border bg-slate-100 rounded-md text-[black]">
+        fetch data
+      </button> */}
+
       <ul className="list-disc p-4">
-        {data &&
+        {/* {data &&
           data.data?.map((product) => (
             <li key={product.id}>
               {product.wine} / {product.winery}
             </li>
-          ))}
+          ))} */}
+        {data && data.map((productName) => <li key={productName}>{productName}</li>)}
       </ul>
     </>
   );
---
+++ react-query-test/src/hooks/useProductName.js
@@ -0,0 +1,18 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchProducts = () => {
+  return axios.get("https://api.sampleapis.com/wines/reds");
+};
+
+// onSuccess, onError 는 이 함수를 호출하는 컴포넌트에서 호출하는게 용이함.
+export const useProductName = (onSuccess, onError) => {
+  return useQuery("get-product", fetchProducts, {
+    onSuccess: onSuccess,
+    onError: onError,
+    select: (data) => {
+      const productName = data.data?.map((p) => p.wine).slice(0, 9);
+      return productName;
+    },
+  });
+};
